2f890a1a94c134ea7fc903d13d1aa5d7345daefe
Saisriswetha <140545509+Saisriswetha@users.noreply.github.com>
2026-08-05 19:58:51 -0400

    Update Background.tsx

--- src/components/Layout/Background.tsx
+++ src/components/Layout/Background.tsx
@@ -1,9 +1,7 @@
-import assetImg from '../../../asset.jpg'
-
 export const Background = () => {
   return (
     <img
-      src="/asset.jpg"
+      src={`${import.meta.env.BASE_URL}asset.jpg`}
       alt="Background"
       className="fixed inset-0 -z-10 w-full h-full object-cover"
     />
